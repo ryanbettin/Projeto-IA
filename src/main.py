139f57e58c62abb7824e14d8f82fa269034a4bf8
ryanbettin <ryanbettin44@gmail.com>
2025-06-10 21:41:57 -0300

    adição das imagens do semafaro

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
-import sys
+import argparse
+from ia_n_uv.utils import detectar_cores_e_formas
 
 def main():
-    print("Hello from ia-uv-semafaro!")
-  
+    parser = argparse.ArgumentParser(description="Detector de Sinal com Formas para Daltônicos")
+    parser.add_argument('--input', required=True, help="Caminho da imagem de entrada")
+    args = parser.parse_args()
+
+    sinais = detectar_cores_e_formas(args.input)
+
+    if sinais:
+        print("Sinais detectados na imagem:")
+        for cor, forma in sinais:
+            print(f"- {cor}: {forma}")
+    else:
+        print("Nenhuma cor de semáforo detectada.")
+
 if __name__ == "__main__":
-    sys.exit(main()) 
+    main()
